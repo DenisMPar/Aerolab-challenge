a35723e116dfa32054d1a6b25ce6ed1b9127ae92
Denis Parada <denispar.dev@gmail.com>
2024-10-30 23:57:43 -0300

    Added: getdetail and date formatter function

--- lib/api.ts
+++ lib/api.ts
@@ -10,3 +10,51 @@ export async function fetchApiGet(path: RequestInfo) {
   if (status >= 400) throw new Error(resJson.message);
   if (status >= 200 && status < 300) return resJson;
 }
+
+export interface GameDetail {
+  id: number;
+  cover?: {
+    id: number;
+    url: string;
+  };
+  rating?: number;
+  first_release_date: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  involved_companies: {
+    id: number;
+    company: {
+      id: number;
+      name: string;
+    };
+  }[];
+  name: string;
+  platforms: {
+    id: number;
+    name: string;
+  }[];
+  screenshots: {
+    id: number;
+    url: string;
+  }[];
+  similar_games: {
+    id: number;
+    cover: {
+      id: number;
+      url: string;
+    };
+  }[];
+  summary: string;
+}
+
+export async function getGameDetails(slug: string): Promise<GameDetail | null> {
+  try {
+    const res = await fetchApiGet(`/game/${slug}`);
+    return res;
+  } catch (error) {
+    console.log("error on fetch :(", error);
+    return null;
+  }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function timeStamptUnixToDate(unix: number) {
+  const date = new Date(unix * 1000);
+  const formattedDate = date.toLocaleDateString("es-ES", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  });
+
+  return formattedDate;
 }
