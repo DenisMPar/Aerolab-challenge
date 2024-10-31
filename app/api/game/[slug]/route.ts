df96efacafb27db21fa73c868a5f76c369d80839
Denis Parada <denispar.dev@gmail.com>
2024-10-31 15:12:04 -0300

    Fixed: added necesary data to query

--- app/api/game/[slug]/route.ts
+++ app/api/game/[slug]/route.ts
@@ -12,7 +12,7 @@ export async function GET(
       Authorization: `Bearer ${process.env.DB_ACCESS_TOKEN}`,
       "Content-Type": "text/plain",
     },
-    body: `fields name,cover.url,first_release_date,involved_companies.company.name,platforms.name,rating,screenshots.url,summary,genres.name,similar_games.cover.url; where slug = "${slug}";`,
+    body: `fields name,slug,cover.url,first_release_date,involved_companies.company.name,platforms.name,rating,screenshots.url,summary,genres.name,similar_games.cover.url,similar_games.slug, similar_games.name; where slug = "${slug}";`,
   });
   const data = await res.json();
 
