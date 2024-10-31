35297c751c0e57f8141818acba2b2ed45bf77218
Denis Parada <denispar.dev@gmail.com>
2024-10-31 15:11:22 -0300

    Added no image case

--- components/game-card/index.tsx
+++ components/game-card/index.tsx
@@ -2,21 +2,35 @@ import Link from "next/link";
 import { Card } from "../ui/card";
 import Image from "next/image";
 import classes from "./index.module.css";
+import { ImageOff } from "lucide-react";
+import { Body1 } from "../ui/typography";
 interface Props {
   gameDetailUrl: string;
   imageUrl?: string;
+  gameName: string;
 }
-export function GameCard({ gameDetailUrl }: Props) {
+export function GameCard({ gameDetailUrl, imageUrl, gameName }: Props) {
   return (
     <Card>
       <Link href={gameDetailUrl}>
-        <Image
-          className={classes.game_card__image}
-          src={"https://images.igdb.com/igdb/image/upload/t_thumb/co2smo.jpg"}
-          alt="game image"
-          width={114}
-          height={152}
-        />
+        {imageUrl ? (
+          <Image
+            className={classes.game_card__image}
+            src={imageUrl}
+            alt="game"
+            width={114}
+            height={152}
+          />
+        ) : (
+          <div>
+            <Body1>{gameName}</Body1>
+            <ImageOff
+              className={classes.game_card__image}
+              width={114}
+              height={152}
+            />
+          </div>
+        )}
       </Link>
     </Card>
   );
