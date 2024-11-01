3e935da44949c601ecd8d7ea47258ef95a26aa15
Denis Parada <denispar.dev@gmail.com>
2024-11-01 16:46:01 -0300

    Added image off variant

--- components/game-card/index.tsx
+++ components/game-card/index.tsx
@@ -1,15 +1,20 @@
+import { ImageOff } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { Card } from "../ui/card";
-import Image from "next/image";
 import classes from "./index.module.css";
-import { ImageOff } from "lucide-react";
-import { Body1 } from "../ui/typography";
 interface Props {
   gameDetailUrl: string;
   imageUrl?: string;
   gameName: string;
+  priority: boolean;
 }
-export function GameCard({ gameDetailUrl, imageUrl, gameName }: Props) {
+export function GameCard({
+  gameDetailUrl,
+  imageUrl,
+  gameName,
+  priority,
+}: Props) {
   const bigCoverUrl = imageUrl?.replace("t_thumb", "t_cover_big");
   return (
     <Card>
@@ -21,15 +26,12 @@ export function GameCard({ gameDetailUrl, imageUrl, gameName }: Props) {
             alt="game"
             width={264}
             height={374}
+            priority={priority}
           />
         ) : (
-          <div>
-            <Body1>{gameName}</Body1>
-            <ImageOff
-              className={classes.game_card__image}
-              width={114}
-              height={152}
-            />
+          <div className={classes.game_card__offImage_container}>
+            <p className={classes.game_card__offImage_title}>{gameName}</p>
+            <ImageOff className={classes.game_card__offImage} />
           </div>
         )}
       </Link>
